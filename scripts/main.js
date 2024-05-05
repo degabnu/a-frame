2a77d87670e12d0656324b687e3a647e655caa3a
Alice Machado Koch <machado.alice.koch@gmail.com>
2024-05-04 21:50:09 -0300

    Coordenadas labirinto

--- scripts/main.js
+++ scripts/main.js
@@ -2,29 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     // Geração da estrutura do labirinto
     generateMaze();
 
-    // Adicionando um ouvinte de eventos ao elemento desafio
+    // Adicionando um ouvinte de eventos ao elemento desafio, !!!por enquanto não ta funcionando!!!
     const challengeElement = document.querySelector('#challenge');
     challengeElement.addEventListener('click', function() {
-        alert('Congratulations! You completed the challenge!');
+        alert('Parabéns! Você completou o desafio!');
     });
 });
 
 function generateMaze() {
     const mazeElement = document.querySelector('#maze');
-    // Coordenadas detalhadas para as paredes do labirinto
-    const walls = [
-        // Contorno externo
-        { x: -5, z: -5 }, { x: -5, z: -4 }, { x: -5, z: -3 }, { x: -5, z: -2 }, { x: -5, z: -1 }, { x: -5, z: 0 }, { x: -5, z: 1 }, { x: -5, z: 2 }, { x: -5, z: 3 }, { x: -5, z: 4 },
-        { x: 5, z: -5 }, { x: 5, z: -4 }, { x: 5, z: -3 }, { x: 5, z: -2 }, { x: 5, z: -1 }, { x: 5, z: 0 }, { x: 5, z: 1 }, { x: 5, z: 2 }, { x: 5, z: 3 }, { x: 5, z: 4 },
-        { x: -4, z: 5 }, { x: -3, z: 5 }, { x: -2, z: 5 }, { x: -1, z: 5 }, { x: 0, z: 5 }, { x: 1, z: 5 }, { x: 2, z: 5 }, { x: 3, z: 5 }, { x: 4, z: 5 },
-        { x: -4, z: -5 }, { x: -3, z: -5 }, { x: -2, z: -5 }, { x: -1, z: -5 }, { x: 0, z: -5 }, { x: 1, z: -5 }, { x: 2, z: -5 }, { x: 3, z: -5 }, { x: 4, z: -5 },
-        // Paredes internas mais densas
-        { x: -4, z: -3 }, { x: -3, z: -3 }, { x: -1, z: -3 }, { x: 0, z: -3 }, { x: 1, z: -3 },
-        { x: 3, z: -1 }, { x: 4, z: -1 }, { x: 0, z: 0 }, { x: -3, z: 1 }, { x: -4, z: 1 },
-        { x: 2, z: 1 }, { x: 3, z: 2 }, { x: 4, z: 3 }, { x: 3, z: 4 },
-        { x: -3, z: 3 }, { x: -2, z: 2 }, { x: -2, z: 3 }, { x: -2, z: 1 }, { x: -3, z: 0 }, { x: -1, z: 0 },
-        { x: 1, z: -2 }, { x: 2, z: -2 }, { x: 1, z: -4 }, { x: 2, z: -4 }, { x: -4, z: 2 }, { x: -4, z: 1 }
-    ];
+
+    // Gerando as coordenadas das paredes do labirinto
+    const walls = generateOuterWalls().concat(generateInnerWalls());
 
     // Gerando as paredes do labirinto
     walls.forEach(function(wallCoords) {
@@ -37,4 +26,28 @@ function generateMaze() {
         wall.setAttribute('static-body', '');
         mazeElement.appendChild(wall);
     });
+
+    function generateOuterWalls() {
+        // Função para gerar as coordenadas das paredes externas de limite do labirinto
+        const outerWalls = [];
+        for (let i = -13; i <= 13; i++) {
+            outerWalls.push({ x: -12, z: i });
+            outerWalls.push({ x: 12, z: i });
+            outerWalls.push({ x: i, z: -13 });
+            outerWalls.push({ x: i, z: 13 });
+        }
+        return outerWalls;
+    }
+    
+    function generateInnerWalls() {
+        // Função para gerar as coordenadas das paredes internas do labirinto
+        const innerWalls = [
+            { x: -10, z: 12 }, { x: -10, z: 11 }, { x: -10, z: 10 }, { x: -10, z: 9 }, { x: -9, z: 9 }, { x: -8, z: 9 }, { x: -8, z: 10 }, { x: -8, z: 11 }, 
+            { x: -11, z: 7 }, { x: -10, z: 7 }, { x: -9, z: 7 }, { x: -8, z: 7 }, { x: -7, z: 7 }, { x: -6, z: 7 }, { x: -6, z: 8 }, { x: -6, z: 9 }, 
+            { x: -6, z: 11 }, { x: -5, z: 11 }, { x: -4, z: 11 }, { x: -4, z: 10 }, { x: -3, z: 10 }, { x: -2, z: 10 }, { x: -1, z: 10 }, { x: -2, z: 12 }, 
+            { x: 0, z: 10 }, { x: 0, z: 11 }, { x: 1, z: 11 }, { x: 2, z: 11 }, { x: 3, z: 11 }, { x: 4, z: 11 }, { x: 4, z: 10 }, { x: -4, z: 1 }, 
+            { x: -3, z: 0 }, { x: -4, z: 2 }, { x: -4, z: 1 }, { x: -3, z: 0 }, { x: -4, z: 2 }, { x: -4, z: 1 }, { x: -3, z: 0 },
+        ];
+        return innerWalls;
+    }
 }
